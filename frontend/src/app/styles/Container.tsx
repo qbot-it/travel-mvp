0464be97ddf499f7b7a1e920a1bcff7ea8983e14
Hien Giang <gianghien2999@gmail.com>
2023-12-08 01:31:25 +0100

    add global styles, theme, container

---
+++ frontend/src/app/styles/Container.tsx
@@ -0,0 +1,12 @@
+// Container.tsx
+'use client'
+import styled from 'styled-components';
+
+const Container = styled.div`
+  width: 100%;
+  max-width: 395px; // Set to the width of iPhone 14
+  margin: 0 auto;
+  /* Add any other styling as needed */
+`;
+
+export default Container;
