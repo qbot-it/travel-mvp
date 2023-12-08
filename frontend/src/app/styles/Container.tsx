f7969f648bfdd38f9336cb71eea8269b1a482724
Hien Giang <gianghien2999@gmail.com>
2023-12-08 02:42:02 +0100

    changed default global style & container styles

--- frontend/src/app/styles/Container.tsx
+++ frontend/src/app/styles/Container.tsx
@@ -4,8 +4,11 @@ import styled from 'styled-components';
 
 const Container = styled.div`
   width: 100%;
+  height: 844px;
   max-width: 395px; // Set to the width of iPhone 14
   margin: 0 auto;
+  margin-color: #e5e5e5;
+  background-color: #FFFFFF; 
   /* Add any other styling as needed */
 `;
 
