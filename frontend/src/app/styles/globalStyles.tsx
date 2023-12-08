820c95ce082beb288ffa8025ba9eb7f20eed36c0
Hien Giang <gianghien2999@gmail.com>
2023-12-08 01:31:25 +0100

    add global styles, theme, container

---
+++ frontend/src/app/styles/globalStyles.tsx
@@ -0,0 +1,14 @@
+// GlobalStyles.ts
+
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyles = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    font-family: 'Arial', sans-serif; /* Choose your preferred font */
+    /* Add any other global styles here */
+  }
+`;
+
+export default GlobalStyles;
